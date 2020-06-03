5986ec583e94c2af17a2e787581bbdffa6cb0e88
Ryan Ho <ryanho@Ryans-MacBook-Pro.local>
2020-06-02 17:39:19 -1000

    all complete

--- server.js
+++ server.js
@@ -22,11 +22,26 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
+
 app.get('/', function (req, res) {
   //res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
   res.status(200).render('tweeterPage', {
     twitData: tData
   });
+});
+
+app.get('/:n', function (req, res, next) {
+  var num = req.params.n;
+  if(num == '0'||num == '1'||num == '2'||num == '3'||num == '4'||num == '5'||num == '6'||num == '7'){
+    //console.log("Number:", num);
+    //console.log(tData[num]);
+    res.status(200).render('tweeterPage', {
+      twitData: [tData[num]]
+    });
+  }
+else{
+  next();
+}
 });
 
 app.get('*', function (req, res) {
